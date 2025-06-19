5018e095d239ef04ed8c565f010bbdb855fbf9c1
Joshf225 <joshuafamose@gmail.com>
2025-06-19 21:14:50 +0100

    fixed boken img_src url link, added loader component

--- backend/index.js
+++ backend/index.js
@@ -15,17 +15,16 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
+const apiKey = process.env.NASA_API_KEY;
+
+//optimise api to grab data from the based on earth_date and store it in localStorage, then if the date is changed we grap photos from localStorage based off the other filters (sol, camers etc..)
+
 app.get("/api/mars-photos", async (req, res) => {
   const { earth_date, rover, cameras, sol } = req.query;
 
-  // console.log("PARAMS: ", earth_date, rover, cameras, sol);
+  console.log("PARAMS: ", earth_date, rover, cameras, sol);
 
-  if (!earth_date || !rover || !cameras || !sol) {
-    return res.status(500).json({ error: "Params is empty" });
-  }
-
-  const apiKey = process.env.NASA_API_KEY;
-  const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=1000&earth_date=${earth_date}&sol=${sol}&cameras=${cameras}&api_key=${apiKey}`;
+  const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${earth_date}&sol=${sol}&cameras=${cameras}&api_key=${apiKey}`;
 
   try {
     const response = await fetch(url, {
@@ -35,9 +34,47 @@ app.get("/api/mars-photos", async (req, res) => {
       },
     });
     const resJson = await response.json();
-    return res.json({ photos: resJson.photos });
+    if (!resJson.photos) {
+      res.status(500).json({
+        error: "Params is empty",
+        data: {
+          Camera: cameras || "missing",
+          Earth_Date: earth_date || "missing",
+          Sol: sol || "missing",
+          Rover: rover || "Rover param is missing and is required",
+        },
+      });
+    }
+    return res.json({ photos: resJson.photos, length: resJson.photos.length });
   } catch (err) {
-    console.error("Error getching from the NASA API: ", err);
-    res.status(500).json({ error: "Failed to fetch photos" });
+    console.error("Error fetching from the NASA API: ", err);
+    res.status(500).json({ error: "Failed to fetch photos", err });
+  }
+});
+
+app.get("/api/manifests", async (req, res) => {
+  const { rover } = req.query;
+  const url = `https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?api_key=${apiKey}`;
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const { photo_manifest } = await response.json();
+    const { max_sol, max_date, landing_date, total_photos } = photo_manifest;
+    res.status(200).json({
+      roverDetails: {
+        landing_date,
+        max_date,
+        max_sol,
+        total_photos,
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching from the NASA API: ", err);
+    res.status(500).json({ error: "Failed to fetch rovers manifest", err });
   }
 });
--- frontend/src/pages/ExploreMars.jsx
+++ frontend/src/pages/ExploreMars.jsx
@@ -1,32 +1,75 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Loader from "../components/Loader";
 
 const ExplorePage = () => {
   const [earthDate, setEarthDate] = useState("");
-  const [rover, setRover] = useState("curiosity");
+  const [rover, setRover] = useState(null);
   const [cameras, setCameras] = useState("navcam");
   const [sol, setSol] = useState(1000);
   const [photos, setPhotos] = useState([]);
+  const [roverDetails, setRoverDetails] = useState(null);
   const [displayPhotos, setDisplayPhotos] = useState([]);
+  const [roverMinDate, setRoverMinDate] = useState("");
+  const [roverMaxDate, setRoverMaxDate] = useState("");
+  const [roverMaxSol, setRoverMaxSol] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
-  const baseUrl = "http://localhost:3000/api/mars-photos?";
+  const baseUrl = "http://localhost:3000/api";
 
   const params = `earth_date=${earthDate}&rover=${rover}&cameras=${cameras}&sol=${sol}`;
-  const url = `${baseUrl}${params}`;
+  const url = `${baseUrl}/mars-photos?${params}`;
 
   const fetchPhotos = async () => {
     // console.log("PARAMS: ", params);
     // console.log("FULL URL: ", url);
     try {
+      setIsLoading(true);
       const res = await fetch(url, {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
       });
-      const data = await res.json();
-      // console.log("DATA FROM BACKEND!! ", res);
-      setPhotos(data.photos || []);
+      const { photos } = await res.json();
+      const workingPhotos = photos?.filter((photo) => {
+        // check the 5+6 characters from the end
+        if (
+          photo.img_src.slice(
+            photo.img_src.length - 6,
+            photo.img_src.length - 4
+          ) !== "BR"
+        ) {
+          return photo;
+        }
+      });
+
+      console.log("DATA FROM BACKEND!! ", photos);
+      setPhotos(workingPhotos || []);
+      setIsLoading(false);
+      // add to photos localStorage
+    } catch (error) {
+      console.error("Error fetching photos:", error);
+    }
+  };
+
+  const fetchRover = async (cappedRover) => {
+    // const roverCap = rover[0].toUpperCase() + rover.substring(1);
+    // console.log("CAPITILISED ROVER: ", roverCap);
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${baseUrl}/manifests?rover=${cappedRover}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const { roverDetails } = await res.json();
+      setRoverDetails(roverDetails);
+      setRoverMaxDate(roverDetails.max_date);
+      setRoverMinDate(roverDetails.landing_date);
+      setRoverMaxSol(roverDetails.max_sol);
+      setIsLoading(false);
     } catch (error) {
       console.error("Error fetching photos:", error);
     }
@@ -36,6 +79,25 @@ const ExplorePage = () => {
     setDisplayPhotos(photos);
     console.log("PHOTOS: ", photos);
   }, [photos]);
+
+  React.useEffect(() => {
+    console.log("Rover Details: ", roverDetails);
+    console.log("Rover Max Date: ", roverMaxDate);
+    console.log("Rover Max Sol: ", roverMaxSol);
+    setSol(0);
+  }, [roverDetails]);
+
+  React.useEffect(() => {
+    // filter displayPhotos based on changed earth date
+    let filteredPhotos = displayPhotos.filter((photos) => {
+      return earthDate === photos.earth_date;
+    });
+    setDisplayPhotos(filteredPhotos);
+  }, [earthDate]);
+
+  React.useEffect(() => {
+    console.log("Display Photos: ", displayPhotos);
+  }, [displayPhotos]);
 
   return (
     <div className="min-h-screen w-full bg-white border">
@@ -52,29 +114,42 @@ const ExplorePage = () => {
       {/* Controls */}
       <section className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-8 bg-white px-11">
         <div>
+          <label className="block text-gray-700 font-medium mb-1">Rover</label>
+          <select
+            onChange={(e) => setRover(e.target.value)}
+            className="w-full border px-3 py-2 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#0c1c3b] bg-transparent text-black border-gray-400"
+          >
+            <option value="">--Please choose a Rover--</option>
+            <option value="curiosity" onClick={() => fetchRover("Curiosity")}>
+              Curiosity
+            </option>
+            <option
+              value="opportunity"
+              onClick={() => fetchRover("Opportunity")}
+            >
+              Opportunity
+            </option>
+            <option value="spirit" onClick={() => fetchRover("Spirit")}>
+              Spirit
+            </option>
+          </select>
+        </div>
+
+        <div className={roverDetails ? "block" : "hidden"}>
           <label className="block text-gray-700 font-medium mb-1">
             Earth Date
           </label>
           <input
             type="date"
             defaultValue={earthDate}
+            min={roverMinDate}
+            max={roverMaxDate}
             onChange={(e) => setEarthDate(e.target.value)}
             className="w-full border px-3 py-2 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#0c1c3b] bg-transparent text-black border-gray-400"
           />
         </div>
-        <div>
-          <label className="block text-gray-700 font-medium mb-1">Rover</label>
-          <select
-            defaultValue={rover}
-            onChange={(e) => setRover(e.target.value)}
-            className="w-full border px-3 py-2 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#0c1c3b] bg-transparent text-black border-gray-400"
-          >
-            <option value="curiosity">Curiosity</option>
-            <option value="opportunity">Opportunity</option>
-            <option value="spirit">Spirit</option>
-          </select>
-        </div>
-        <div>
+
+        <div className={roverDetails ? "block" : "hidden"}>
           <label className="block text-gray-700 font-medium mb-1">Camera</label>
           <select
             defaultValue={cameras}
@@ -88,7 +163,8 @@ const ExplorePage = () => {
             <option value="chemcam">CHEMCAM</option>
           </select>
         </div>
-        <div className="flex items-end">
+
+        <div className={`flex items-end ${roverDetails ? "block" : "hidden"}`}>
           <button
             onClick={fetchPhotos}
             className="w-full bg-[#1d3e8a] text-white py-2 rounded-md font-semibold hover:bg-[#15316d] transition"
@@ -98,43 +174,6 @@ const ExplorePage = () => {
         </div>
       </section>
 
-      {/* Photo Grid */}
-      <section className="px-11">
-        {photos.length > 0 ? (
-          <>
-            <h2 className="text-xl font-medium text-gray-800 mb-2">
-              {rover} | {cameras} | Sol {sol}
-            </h2>
-
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mb-6">
-              {displayPhotos.length > 9
-                ? displayPhotos
-                    .slice(0, 10)
-                    .map((photo, index) => (
-                      <img
-                        key={index}
-                        src={photo.img_src}
-                        alt="Mars"
-                        className="w-full h-40 object-cover rounded shadow cursor-pointer"
-                      />
-                    ))
-                : displayPhotos.map((photo, index) => (
-                    <img
-                      key={index}
-                      src={photo.img_src}
-                      alt="Mars"
-                      className="w-full h-40 object-cover rounded shadow cursor-pointer"
-                    />
-                  ))}
-            </div>
-          </>
-        ) : (
-          <p className="text-gray-600">
-            No photos found on this Sol date. Try another date or camera.
-          </p>
-        )}
-      </section>
-
       {/* Sol Slider */}
       <section className="mt-6">
         <div className="w-full flex items-center justify-center gap-5">
@@ -142,13 +181,62 @@ const ExplorePage = () => {
           <input
             type="range"
             min="0"
-            max="1000"
+            max={roverMaxSol || 1000}
             defaultValue={sol}
             onChange={(e) => setSol(e.target.value)}
             className="w-1/2 text-bl"
           />
-          <p className="text-sm text-gray-600">Selected Sol: {sol}</p>
+          <p className="text-sm text-gray-600">
+            Selected Sol: {sol}/{roverMaxSol || 1000}
+          </p>
         </div>
+      </section>
+
+      {/* Photo Grid */}
+      <section className="px-11">
+        {!isLoading ? (
+          <>
+            {photos.length > 0 ? (
+              <>
+                <h2 className="text-xl font-medium text-gray-800 mb-2">
+                  {rover} | {cameras} | Sol {sol}
+                </h2>
+
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mb-6">
+                  {displayPhotos.length > 9
+                    ? displayPhotos.slice(0, 10).map((photo, index) => (
+                        <a href={photo.img_src} target="_blank">
+                          <img
+                            key={index}
+                            src={photo.img_src}
+                            alt="Mars"
+                            className="w-full h-40 object-cover rounded shadow cursor-pointer"
+                          />
+                        </a>
+                      ))
+                    : displayPhotos.map((photo, index) => (
+                        <a href={photo.img_src} target="_blank">
+                          <img
+                            key={index}
+                            src={photo.img_src}
+                            alt="Mars"
+                            className="w-full h-40 object-cover rounded shadow cursor-pointer"
+                          />
+                        </a>
+                      ))}
+                </div>
+              </>
+            ) : (
+              <p className="text-gray-600">
+                No photos found on this Sol date. Try another date or camera.
+              </p>
+            )}
+          </>
+        ) : (
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mb-6">
+            <Loader />
+          </div>
+        )}
       </section>
     </div>
   );
